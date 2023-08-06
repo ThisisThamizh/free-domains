1ed0f4a65abfd5f10c3b13cb06200aa016c75d13
Thamizh <91716052+ThisisThamizh@users.noreply.github.com>
2023-08-06 18:38:47 +0530

    create pakathu-kadai.is-an.app.js

---
+++ domains/pakathu-kadai.is-an.app.js
@@ -0,0 +1,14 @@
+
+addSubDomain({
+    description: 'A portfolio website for a e-bussines organization, built with bootstrap CSS',
+    domain: 'is-an.app', 
+    subdomain: 'pakathu-kadai.is-an.app', 
+    owner: {
+      repo: 'https://github.com/ThisisThamizh/pakathu-kadai',
+      email: 'venthank88@gmail.com',
+    },
+    record: {
+      CNAME: 'thisisthamizh.github.io',
+      TXT: ['6d62fab77bd02e3bc3b3acaac149d2'],
+    },
+  })
